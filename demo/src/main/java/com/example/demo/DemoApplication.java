64b79583088177e8091d93b296998fcdea5d19f4
MTharun <tharun.m@unisys.com>
2025-09-10 13:08:42 +0530

    added connection string

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.service.DocumentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
